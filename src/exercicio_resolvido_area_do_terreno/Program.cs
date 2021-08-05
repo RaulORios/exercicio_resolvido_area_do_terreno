6b9594eafc87aca9c88b7245bcb3f86e091c5bb9
RaulORios <raul.orios89@outlook.com>
2021-08-05 11:55:29 -0300

    Program.cs adicionado

---
+++ src/exercicio_resolvido_area_do_terreno/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_resolvido_area_do_terreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura;
+            double comprimento;
+            double precometroquadrado;
+            double area;
+            double preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precometroquadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precometroquadrado;
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Area = {area.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Preço = {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
